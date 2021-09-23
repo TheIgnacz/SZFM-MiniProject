0d62581cb3387c1d6370a5c0c1965d678f1e897a
György David <gyorgydavid2000@gmail.com>
2021-09-24 02:31:41 +0300

    Java script kód különszedve és kezdőoldal létrehozva

---
+++ src/app.js
@@ -0,0 +1,31 @@
+function kep(){
+    var kep = document.urlap.hatter.value
+    console.log(kep)
+    if (kep=="ejszaka.jpg"){ document.body.style.color = "white"}
+    kep = "url(kepek/"+kep+")"
+    document.body.style.backgroundImage = kep
+    }
+function ido(){
+    var most = new Date();
+    var ora = most.getHours();
+    var perc = most.getMinutes();
+    var mp = most.getSeconds();
+    var idostr = ora;
+    idostr += ((perc < 10) ? ":0" : ":") + perc;   
+    idostr += (((mp < 10 ) ? ":0" : ":") + mp);
+    document.querySelector('.ora').innerText = idostr;
+    setTimeout("ido()",1000);
+}
+function viccek(viccdata){
+    fetch(viccdata)
+        .then(response => response.json())
+        .then(function (data) {
+        for(var i in data){
+            var div = document.createElement('div');
+            var parent = document.getElementById("vicct");
+            parent.appendChild(div)
+            div.className = "vicc";
+            div.innerHTML = data[i].text; 
+        };
+        })
+}
